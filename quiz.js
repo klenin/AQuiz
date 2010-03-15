6fef42f9e2e27b11fabb081e1c07dce6c47eb104
Alexander S. Klenin <klenin@gmail.com>
2010-03-15 11:10:45 +1000

    Use jQuery inheritance plugin

--- quiz.js
+++ quiz.js
@@ -1,14 +1,25 @@
-function SingleChoiceQuestion(src) {
-    this.text = src.text;
-    this.variants = new Array();
-    if (src.variants)
-        for (var i = 0; i < src.variants.length; ++i)
-            this.variants.push(src.variants[i]);
-    this.answer = null;
-    
-    this.ui = function () { return $('#singleChoice'); }
 
-    this.show = function () {
+var ChoiceQuestion = $.inherit(
+{
+    __constructor: function(src) {
+        this.text = src.text;
+        this.variants = new Array();
+    },
+});
+
+var SingleChoiceQuestion = $.inherit(ChoiceQuestion,
+{
+    __constructor: function(src) {
+        this.__base(src);
+        if (src.variants)
+            for (var i = 0; i < src.variants.length; ++i)
+                this.variants.push(src.variants[i]);
+        this.answer = null;
+    },
+    
+    ui: function() { return $('#singleChoice'); },
+
+    show: function() {
         var ui = this.ui();
         ui.children('.questionText').text(this.text);
         var variants = ui.children('ul');
@@ -21,27 +32,29 @@ function SingleChoiceQuestion(src) {
             variants.append(v);
         }
         ui.show();
-    };
+    },
 
-    this.rememberAnswer = function() {
+    rememberAnswer: function() {
         var that = this;
         $('#singleChoice input').each(function(i) {
             if (this.checked)
                 that.answer = this.id;
         });
-    };
-}
+    },
+});
 
-function MultiChoiceQuestion(src) {
-    this.text = src.text;
-    this.variants = new Array();
-    if (src.variants)
-        for (var i = 0; i < src.variants.length; ++i)
-            this.variants.push({ text: src.variants[i], checked: false });
-    
-    this.ui = function () { return $('#multiChoice'); }
+var MultiChoiceQuestion = $.inherit(ChoiceQuestion,
+{
+    __constructor: function(src) {
+        this.__base(src);
+        if (src.variants)
+            for (var i = 0; i < src.variants.length; ++i)
+                this.variants.push({ text: src.variants[i], checked: false });
+    },
 
-    this.show = function () {
+    ui: function() { return $('#multiChoice'); },
+
+    show: function() {
         var ui = this.ui();
         ui.children('.questionText').text(this.text);
         var variants = ui.children('ul');
@@ -54,53 +67,55 @@ function MultiChoiceQuestion(src) {
             variants.append(v);
         }
         ui.show();
-    };
+    },
 
-    this.rememberAnswer = function() {
+    rememberAnswer: function() {
         var that = this;
         $('#multiChoice input').each(function(i) {
             if (this.id)
                 that.variants[this.id - 1].checked = this.checked;
         });
-    };
+    },
+});
 
-}
+var Quiz = $.inherit(
+{
+    __constructor: function(questions) {
+        this.questions = questions;
+        this.currentQuestion = 0;
+    },
 
-function Quiz(questions) {
-    this.questions = questions;
-    this.currentQuestion = 0;
-
-    this.nextOk = function () {
+    nextOk: function() {
         return this.currentQuestion < this.questions.length - 1;
-    };
+    },
 
-    this.prevOk = function () {
+    prevOk: function() {
         return this.currentQuestion > 0;
-    };
+    },
 
-    this.nextQuestion = function () {
+    nextQuestion: function() {
         if (!this.nextOk()) return;
         this.leaveQuestion();
         ++this.currentQuestion;
         this.showQuestion();
-    };
+    },
 
-    this.prevQuestion = function () {
+    prevQuestion: function() {
         if (!this.prevOk()) return;
         this.leaveQuestion();
         --this.currentQuestion;
         this.showQuestion();
-    };
+    },
 
-    this.leaveQuestion = function() {
+    leaveQuestion: function() {
         var q = this.questions[this.currentQuestion];
         q.ui().hide();
         q.rememberAnswer();
-    }
+    },
 
-    this.showQuestion = function () {
+    showQuestion: function() {
         this.questions[this.currentQuestion].show();
         $('nextQuestion').disabled = !this.nextOk();
         $('prevQuestion').disabled = !this.prevOk();
-    };
-};
+    },
+});
